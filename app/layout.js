6edbbcce694bfb7b71c96d99e110b26e6e03b6ea
Kike0013 <kique0013@hotmail.com>
2023-05-16 15:33:23 -0400

    [add] animated background

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
+"use client"
+import { useEffect } from "react"
+import { Source_Code_Pro } from 'next/font/google'
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Source_Code_Pro({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -9,6 +11,33 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
+  useEffect(() => {
+    const threeScript = document.createElement("script");
+    const vantaScript = document.createElement("script");
+    threeScript.setAttribute("id", "threeScript");
+    vantaScript.setAttribute("id", "vantaScript");
+    threeScript.setAttribute(
+      "src",
+      "https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
+    );
+    vantaScript.setAttribute(
+      "src",
+      "https://cdn.jsdelivr.net/npm/vanta/dist/vanta.waves.min.js"
+    );
+
+    document.getElementsByTagName("head")[0].appendChild(threeScript)
+    document.getElementsByTagName("head")[0].appendChild(vantaScript)
+
+    return () => {
+      if (threeScript || vantaScript) {
+        threeScript.remove()
+        vantaScript.remove()
+      }
+    }
+  },
+    [])
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
--- app/page.js
+++ app/page.js
@@ -1,8 +1,50 @@
+"use client"
+import { useEffect, useRef, useState } from 'react';
+import NET from "vanta/dist/vanta.net.min";
+import * as THREE from "three";
+// import SectionCard from './components/SectionCard';
 
 export default function Home() {
+
+  const [bgEffect, setBgEffect] = useState(0)
+  const bgRef = useRef(null)
+
+  useEffect(() => {
+    if (!bgEffect) {
+      setBgEffect(
+        NET({
+          el: bgRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          color: 0x4ec9b0,
+          backgroundColor: 0x1f1f1f,
+          points: 20.00,
+          maxDistance: 21.00,
+          spacing: 20.00,
+          showDots: false
+        })
+      )
+    }
+
+    return () => {
+      if (bgEffect) bgEffect.destroy()
+    }
+
+  }, [bgEffect])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <div>
+      <main ref={bgRef}>
+        <div  className='h-screen'>
+          {/* <SectionCard /> */}
+        </div>
+      </main>
+    </div>
   )
 }
