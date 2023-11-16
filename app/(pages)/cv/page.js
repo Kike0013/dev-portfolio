de098eca59bf0151ced6cb61b9141b0bc8f52da6
Kike0013 <kique0013@hotmail.com>
2023-11-16 10:50:47 -0400

    [style] Change CV

--- app/(pages)/cv/page.js
+++ app/(pages)/cv/page.js
@@ -7,22 +7,13 @@ const Cv = () => {
   return (
     <>
       <ContentContainer>
-        <div className="flex flex-col-reverse justify-center sm:flex-row"
+        <div className="flex flex-col-reverse justify-center md:flex-row"
           style={{
           }}
         >
           <div
-            className="
-            w-full
-
-            md:w-3/4
-
-            lg:w-2/4
-
-            xl:w-2/6
-          "
+            className="w-full md:w-3/4 xl:w-2/5 flex"
             style={{
-              display: "flex",
               height: "700px",
               boxShadow: "0 2px 8px 0 rgba(63, 69, 81, 0.16)",
               borderRadius: "8px",
@@ -30,25 +21,24 @@ const Cv = () => {
               overflow: "auto"
             }}
           >
-            <iframe loading="lazy"
+            <iframe
+              loading="lazy"
+              src="https://docs.google.com/document/d/e/2PACX-1vSSFDdqbw0jVqytNME4WgwbEYw8YnhpPeLVbfqbhjuK9LjKVfMHlG9EfDTJQhW5B4mToab9HA_qRNr1/pub?embedded=true"
               className="w-full"
-              style={{
-                height: "700px"
-              }}
-              src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFVg35GM28&#x2F;view?embed">
+            >
             </iframe>
           </div>
-          <div className="flex flex-row justify-center gap-3 p-4 sm:flex-col">
+          <div className="flex flex-row justify-center gap-3 p-4 md:flex-col">
             <a
-              href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFVg35GM28&#x2F;view?utm_content=DAFVg35GM28&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link"
+              href="https://docs.google.com/document/d/1MPYlGkIhpV-BmMw8yrWCr9BR-5FAPPgBF5y0hEfhBm8/edit?usp=sharing"
               target="_blank"
               rel="noopener">
-              <MdOutlineOpenInNew size={70} color="#FFC612" />
+              <MdOutlineOpenInNew size={40} color="#FFC612" />
             </a>
             <a
               href="https://drive.google.com/drive/folders/1_0b8fcoQEnJFNJTAKJLNK9PDwZaO0SPo?usp=sharing"
               target="_blank">
-              <FaFileDownload size={70} color="#FFC612" />
+              <FaFileDownload size={35} color="#FFC612" />
             </a>
           </div>
         </div>
